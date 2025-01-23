7d17fa7b47652a25ebb08fcef5f9b46a372a2634
saimter <saimter@naver.com>
2025-01-23 15:14:27 +0900

    변수, 자료형 선언한기

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace MyCSharp
     {
         static void Main(string[] args)
         {
+            byte x = 65;
+
+            char c = 'A';
+
+            short y = 256;
+
+            float z = 10f;
+
+            double d = 10;
+
+            int a;
+
+            a = 256;
+
+            //string n = Console.ReadLine();
+            //x = int.Parse(n);
+
+
+            Console.WriteLine(x);
+            //Console.WriteLine(args[x]);
+            Console.WriteLine(y);
+
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(c);
+
+            Console.WriteLine(d);
+
+            Console.WriteLine(a);
+
+            Console.WriteLine(3 + x);
+
+
 
         }
     }
