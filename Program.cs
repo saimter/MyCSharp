6db4d7f02863fa774f30200b0acd1592c5d8f305
saimter <saimter@naver.com>
2025-01-23 14:31:34 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace MyCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
+
+//  OOP
+
+
+// 단축키
+//  ctrl + z
+//  ctrl + y
+//  ctrl + K, ctrl + D
+//  ctrl + c
+
+
